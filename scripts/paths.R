4e6912c9e3614a708f2ae683a70d1b06cc0e14f0
aj <ahmed24633@gmail.com>
2023-08-07 12:29:53 +0300

    completed run_analysis.R (used to be tidyup.R)

--- scripts/paths.R
+++ scripts/paths.R
@@ -4,8 +4,8 @@ activity_label_path<-file.path(base_path,"activity_labels.txt")
 
 test_base_path<- file.path(base_path,"test")
 test_paths <- c(file.path(test_base_path,"X_test.txt"),file.path(test_base_path,"subject_test.txt"),file.path(test_base_path,"y_test.txt"))
-names(test_paths)<-c("data","subjects","activiy")
+names(test_paths)<-c("data","subject","activiy")
 
 train_base_path<- file.path(base_path,"train")
 train_paths <- c(file.path(train_base_path,"X_train.txt"),file.path(train_base_path,"subject_train.txt"),file.path(train_base_path,"y_train.txt"))
-names(train_paths)<-c("data","subjects","activiy")
+names(train_paths)<-c("data","subject","activiy")
