abcdb71df2c54d8b1381cb6e40176da598c449e1
aj <ahmed24633@gmail.com>
2023-08-07 11:11:14 +0300

    redid paths

--- scripts/tidyup.R
+++ scripts/tidyup.R
@@ -4,7 +4,11 @@ features<-read.table(features_path)
 cols_to_read<-grep("((-mean)|(-std))\\(\\)",features[,2])
 col_names<-grep("((-mean)|(-std))\\(\\)",features[,2],value = TRUE)
 test_dt<-fread(test_dataset_path,select = cols_to_read)
-test_dt<-cbind(test_subjects_path,test_dt)
+test_act<-fread(test_activity_path)
+labels<-fread(activity_label_path)
+named_activity<-sapply(test_act[,1], function(x){ return(labels[x,2])})[[1]]
+test_dt<-cbind(named_activity,fread(test_subjects_path),test_dt)
+
 #test_dt[0]<-fread()
 #dt<- rbind(fread(test_data_set,select = cols_to_read),fread(train_data_path,select = cols_to_read))
 #colnames(dt)<-col_names
